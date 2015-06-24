fd80f4a85ddff70929d0faf46c99206d4c13be6d
Shafeman <amazingshafeman@yahoo.com>
2015-06-23 19:50:08 -0500

    Fixing sync issues hopefully

--- BarbarianTheater/src/theater/ClientsList.java
+++ BarbarianTheater/src/theater/ClientsList.java
@@ -1,37 +1,22 @@
-/**
- * Barbarians: Douglas Brian Shaffer, Nathan Kangas, Johnathan Franco
- * This class holds a list of Clients for the Theater. It maintains a list
- * and has methods for adding, removing, searching and listing Clients,
- * as well as saving and loading the list.
- */
 package theater;
 
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-
-@SuppressWarnings("serial")
 public class ClientsList implements Serializable {
 
 	private static ClientsList singletonClientList;
     //private static ClientsList singletonClientList = ClientsList.clientListInstance();
     private List<Client> clients;
-    
-    /**
-	 * ClientsList Constructor, creates a singleton list as an ArrayList.
-	 */
+
 	private ClientsList() {
 
 		clients = new ArrayList<Client>();
 	}
 
-	/**
-	 * clientListInstance is used to create a single ClientList if one does
-	 * not exist, maintaining the ClientList's singleton property
-	 * @return
-	 */
 	public static ClientsList clientListInstance() {
 		if(singletonClientList == null){
 			singletonClientList = new ClientsList();
@@ -39,26 +24,17 @@ public class ClientsList implements Serializable {
 		return singletonClientList;
 	}
 
-	/**
-	 * add takes a Client and adds it to the list.
-	 * @param client
-	 * @return
-	 */
 	public boolean add(Client client) {
 		return clients.add(client);
 	}
 	
-	/**
-	 * remove takes a client ID as a string and searches for it in the list.
-	 * If the ID is found, that client is removed, provided the client has
-	 * no future shows scheduled. remove returns true if the client was removed
-	 * and false otherwise.
-	 * @param id
-	 * @return
-	 */
 	public boolean remove(String id){
+		
+		
+		
 		for(int i = 0; i < clients.size(); i++){
-			if(clients.get(i).getID().equals(id)){
+			String idToCheck = clients.get(i).getID();
+			if(idToCheck.equals(id)){
 				clients.remove(i);
 				return true;
 			}				
@@ -66,21 +42,12 @@ public class ClientsList implements Serializable {
 		return false;
 	}
 
-	/**
-	 * getList provides a List of clients.
-	 * @return
-	 */
 	public List<Client> getList() {
 		return clients;
 	}
 	
-	/**
-	 * search takes a Client ID as a string and returns that Client
-	 * if found in the list. search returns null if the client isn't
-	 * found in the list.
-	 * @param id
-	 * @return
-	 */
+
+	
 	public Client search(String id){
 		for(int i = 0; i < clients.size(); i++){
 			if(clients.get(i).getID().equals(id)){
@@ -90,10 +57,6 @@ public class ClientsList implements Serializable {
 		return null;
 	}
 
-	/**
-	 * writeOjbect is used to write the the ClientList to an output stream.
-	 * @param output
-	 */
     private void writeObject(java.io.ObjectOutputStream output) {
         try {
             output.defaultWriteObject();
@@ -103,20 +66,10 @@ public class ClientsList implements Serializable {
         }
     }
 
-    /**
-     * readResolve returns the singleton ClientList
-     * @return
-     */
     private Object readResolve() {
         return singletonClientList;
     }
 
-    /**
-     * readObject is used when loading the ClientList. It takes an ObjectInputStream
-     * and ensures the singleton property is upheld if the stream has
-     * a ClientList.
-     * @param input
-     */
     private void readObject(java.io.ObjectInputStream input) {
         try {
             if (singletonClientList != null) {
