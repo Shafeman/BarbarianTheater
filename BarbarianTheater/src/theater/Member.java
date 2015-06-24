3f07a4abb8b9b483ce43582755385d89bab09ab9
Shafeman <amazingshafeman@yahoo.com>
2015-06-23 20:09:12 -0500

    Documentation to Member

--- BarbarianTheater/src/theater/Member.java
+++ BarbarianTheater/src/theater/Member.java
@@ -1,12 +1,19 @@
+/**
+ * Barbarians: Douglas Brian Shaffer, Nathan Kangas, Johnathan Franco
+ * The Member class acts as a customer in the Theater. The class extends
+ * Theater Patron and maintains a list of Credit Cards that belong to the
+ * Member. It has methods for adding and removing credit cards and generating
+ * a credit card list.
+ */
 package theater;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 
+@SuppressWarnings("serial")
 public class Member extends TheaterPatron implements Serializable {
 
 	private static int CC_NOT_HERE = 0;
@@ -14,25 +21,45 @@ public class Member extends TheaterPatron implements Serializable {
 	private static int SUCCESS = 2;
 	private List<CreditCard> creditCards;
 	
+	/**
+	 * Member constructor
+	 * @param name
+	 * @param address
+	 * @param phoneNumber
+	 * @param creditCardNumber
+	 * @param expiration
+	 */
 	public Member(String name, String address, String phoneNumber, String creditCardNumber, Calendar expiration){
 		super(name, address, phoneNumber);
 		creditCards = new ArrayList<CreditCard>();
 		creditCards.add(new CreditCard(creditCardNumber, expiration));
 	}
 	
+	/**
+	 * addCreditCard adds a credit card to the credit card list.
+	 * @param creditCardNumber
+	 * @param expiration
+	 * @return
+	 */
 	public CreditCard addCreditCard(String creditCardNumber, Calendar expiration){
 		CreditCard card = new CreditCard(creditCardNumber, expiration);
 		creditCards.add(card);
 		return card;
 	}
 
+	/**
+	 * getCreditCards returns a List of credit cards.
+	 * @return
+	 */
 	public List<CreditCard> getCreditCards() {
 		return creditCards;
 	}
 		
 	/**
 	 * Searches the list of creditCards the Member currently holds for 
-	 * the creditCardNumber provided, and removes it. 
+	 * the creditCardNumber provided, and removes it. The method will only
+	 * remove a credit card if the member has more than one card on file.
+	 * A member always needs at least one card on file.
 	 * @param creditCardNumber
 	 * @return true : if found and removed 
 	 * 	false: if not found or Member only has one creditCard	
@@ -52,6 +79,10 @@ public class Member extends TheaterPatron implements Serializable {
 		return CC_NOT_HERE;		
 	}
 
+	/**
+	 * Generates a string of credit cards on file for use in the toString method.
+	 * @return
+	 */
 	private String printCreditCardList() {
 
 		String creditCardList = "";
@@ -63,6 +94,9 @@ public class Member extends TheaterPatron implements Serializable {
 		return creditCardList;
 	}
 
+	/**
+	 * returns a String of the Member's name, address, phone number and credit cards on file.
+	 */
 	@Override
 	public String toString() {
 		return super.toString() + "\n" + printCreditCardList();
